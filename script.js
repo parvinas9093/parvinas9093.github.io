454c4903b396c1b9c715a4f03df540cc694c4a5d
parvinas9093.github.io <parvinas7@nycstudents.net>
2024-10-14 15:49:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Parvina Shamsieva"; // update the part BETWEEN the "quotes"
+let userName = "parvinas9093"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
